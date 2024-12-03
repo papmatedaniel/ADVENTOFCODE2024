20697bedf345644bb3102bff3e0a51d1b29e0660
papmatedaniel <90318564+papmatedaniel@users.noreply.github.com>
2024-12-03 19:45:45 +0100

    Create day1.cs

---
+++ day1.cs
@@ -0,0 +1,64 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics.CodeAnalysis;
+
+
+class Program
+{
+	static void Main(string[] args)
+	{
+
+		string[] beolvas = File.ReadAllLines("input.txt", Encoding.UTF8);
+		List<int> szamok1 = new List<int>();
+		List<int> szamok2 = new List<int>();
+
+		foreach (var item in beolvas)
+		{
+			szamok1.Add(Convert.ToInt32(item.Split("  ")[0]));
+			szamok2.Add(Convert.ToInt32(item.Split("  ")[1]));
+		}
+
+		szamok1 = Sorter(szamok1);
+		szamok2 = Sorter(szamok2);
+		int osszeg = 0;
+		for (int i = 0; i < szamok1.Count; i++)
+		{
+			int ketosszeg = 0;
+
+			ketosszeg = szamok1[i] - szamok2[i];
+
+
+			if (ketosszeg < 0)
+			{
+				ketosszeg = ketosszeg * -1;
+			}
+			osszeg += ketosszeg;
+		}
+
+        Console.WriteLine(osszeg);
+		Console.WriteLine("Nyomj entert");
+		Console.ReadKey();
+	}
+	static List<int> Sorter(List<int> lista)
+	{
+		for (int l = 0; l < lista.Count - 1; l++)
+		{
+			for (int i = 0; i < lista.Count - 1; i++)
+			{
+				if (lista[i] > lista[i + 1])
+				{
+					int segedvaltozo = lista[i + 1];
+					lista[i + 1] = lista[i];
+					lista[i] = segedvaltozo;
+				}
+			}
+		}
+
+		return lista;
+	}
+
+}
